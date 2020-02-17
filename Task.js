98638d9942fe9b72cfd32e064b400f63d2c0705e
Vinicius Dias <vdias6849@gmail.com>
2020-02-17 15:56:57 -0300

    corrigido as funcoes de apagar e finalizar

--- Task.js
+++ Task.js
@@ -10,6 +10,51 @@ class Task {
     }
 }
 
+//adiciona uma nova tarefa com base no que foi digitado
+function addTask() {
+    let input = getInputText().split("em");
+    let task = new Task(input[0], input[1].slice(1));
+    saveInLocalStorage(task);
+    clearInput();
+    renderTasks();
+}
+
+//remove uma tarefa de acordo com o ID passado
+function removeTask(taskID) {
+    try {
+        localStorage.removeItem(taskID);
+    } catch (error) {
+        alert("Erro ao apagar essa tarefa! ");
+        console.log(error);
+    }
+    renderTasks();
+}
+
+//finaliza uma tarefa de acordo com o ID passados
+function finishTask(taskID) {
+    try {
+        let task = JSON.parse(localStorage.getItem(taskID));
+        task.isFinished = true;
+        localStorage.setItem(taskID, JSON.stringify(task));
+    } catch (error) {
+        alert("Erro ao concluir essa tarefa! ");
+        console.log(error);
+    }
+    renderTasks();
+}
+
+//pega todas as tarefas do localstorage
+function getTasks() {
+    let keys = getKeys();
+    let savedTasks = [];
+    for (i in keys) {
+        savedTasks.push(JSON.parse(localStorage.getItem(keys[i])));
+    }
+
+    return savedTasks;
+}
+
+//controlar keys do local storage, esta função retorna a lista de chaves dos objetos ordenadas
 function getKeys() {
     const storedObjects = Object.keys(localStorage);
     let keys = [];
@@ -19,28 +64,18 @@ function getKeys() {
             keys.push(parseInt(storedObjects[key]));
         }
     }
-    return keys;
+    return keys.sort((a, b) => (a > b ? 1 : -1));
 }
 
+// Salva a tarefa no local storage com key unica e incremental
 function saveInLocalStorage(task) {
     let keys = getKeys();
     const maxKey = Math.max(...keys) != -Infinity ? Math.max(...keys) : 0;
-    window.localStorage.setItem(maxKey, JSON.stringify(task));
+    window.localStorage.setItem(maxKey + 1, JSON.stringify(task));
 }
 
-function addTask() {
-    let input = getInputText().split("em");
-    let task = new Task(input[0], input[1]);
-    saveInLocalStorage(task);
-    clearInput();
-    renderTasks();
-}
-
+//renderiza as tarefas na tabela
 function renderTasks() {
-    let keys = getKeys();
-    let savedTasks = [];
-    for (i in keys) {
-        savedTasks.push(JSON.parse(localStorage.getItem(keys[i])));
-    }
+    let savedTasks = getTasks();
     addTasksInTable(savedTasks);
 }
--- interface.js
+++ interface.js
@@ -4,38 +4,54 @@ function addAttrs() {
         .setAttribute("onclick", "addTask()");
 }
 
+//pega o texto que foi digitado no input
 function getInputText() {
     return document.querySelector('input[name="new_task"]').value;
 }
 
+//limpa o texto digitado no input
 function clearInput() {
     document.querySelector('input[name="new_task"]').value = "";
 }
 
+//limpa os dados da tabela
 function clearBody() {
     document.querySelector("#bodyTable").innerHTML = "";
 }
 
-function checkChilds(element) {
-    let firstChild = element.firstElementChild;
-    if (firstChild != null) {
-        let child = firstChild.firstElementChild;
-        return child.innerHTML != "Nenhuma tarefa encontrada";
-    }
-    return false;
-}
-
-function addBody({ name, date, isFinished }) {
-    clearBody();
-
+//Adiciona os elementos no corpo da tabela, criando dinamicamente os elementos
+function addBody({ name, date = "--/--", isFinished }, taskID) {
     let body = document.querySelector("#bodyTable");
     let tr = document.createElement("tr");
     let tDate = document.createElement("td");
     let tName = document.createElement("td");
     let tdActions = document.createElement("td");
 
-    tName.innerHTML = name;
-    tDate.innerHTML = date.slice(0, 6);
+    if (!isFinished) {
+        let finishButton = document.createElement("button");
+        let trashButton = document.createElement("button");
+        let divButtons = document.createElement("div");
+        divButtons.classList.add("btn-group");
+        divButtons.setAttribute("role", "group");
+        divButtons.setAttribute("aria-label", "First group");
+
+        finishButton.title = "Concluir";
+        finishButton.classList.add("btn", "btn-sm", "btn-outline-success");
+        finishButton.setAttribute("onclick", `finishTask(${taskID})`);
+        finishButton.setAttribute("id", "finishButton");
+
+        divButtons.appendChild(finishButton);
+
+        trashButton.title = "Apagar";
+        trashButton.classList.add("btn", "btn-sm", "btn-outline-danger");
+        trashButton.setAttribute("onclick", `removeTask(${taskID})`);
+        trashButton.setAttribute("id", "trashButton");
+        divButtons.appendChild(trashButton);
+
+        tdActions.appendChild(divButtons);
+    }
+    tName.innerHTML = `${name}`;
+    tDate.innerHTML = `${date.slice(0, 5)}`;
     //tdActions = botoes com a propriedade onclick, removeTask(numero da tarefa) - localStorage.removeItetm(tal)
 
     tr.appendChild(tDate);
@@ -45,8 +61,10 @@ function addBody({ name, date, isFinished }) {
     body.appendChild(tr);
 }
 
+//adiciona as tarefas na tabela chamando a funcao addBody dentro de um loop com base na lista de objetos
 function addTasksInTable(savedTasks) {
+    clearBody();
     for (let task in savedTasks) {
-        addBody(savedTasks[task]);
+        addBody(savedTasks[task], parseInt(task) + 1);
     }
 }
